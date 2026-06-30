a3b521edfe65f8c4678f8384db2f7efdcc8b21d6
Rahini <rahinikalra6@gmail.com>
2026-06-30 21:47:54 +0530

    codes in java

--- abc.java
+++ abc.java
@@ -1,5 +1,40 @@
 public class abc {
+        // static int a ;
+    // static byte b ;
+    // static short c ;
+    // static long d ;
+    // static boolean e ;
+    // static char f ; 
+    // static double g;
+    // static float h ; 
+    static int  n= 10 ;
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+         // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+        // System.out.println(e);
+        // System.out.println(f);
+        // System.out.println(g);
+        // System.out.println(h);
+        // if(n%2==0){
+        //     System.out.println("number is even");
+        // }
+        // else{
+        //     System.out.println("number is odd");
+        // }
+        // if(a>b && a>c){
+        //     System.out.println(a);
+        // }
+        // else if(b>a && b>c){
+        //     System.out.println(b);
+        // }
+        // else{
+        //     System.out.println(c);
+        // }
+        String result = n%2==0 ? "even": "odd" ;
+        System.out.println(result);
     }
 }
+
