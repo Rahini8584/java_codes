250171c7405a67d89817999d3e7f5dcbc031a748
Rahini <rahinikalra6@gmail.com>
2026-07-02 15:11:07 +0530

    function overloading

--- Student.java
+++ Student.java
@@ -10,5 +10,15 @@ public class Student {
         System.out.println("Name of Student : \n"+obj.studentname);
         System.out.println("Age of Student : \n"+obj.age);
 
+        Student obj1 = new Student() ;
+        obj1.fxn();
+        obj1.fxn(12);
+
+    }
+    void fxn(){
+        System.out.println("hello from fxn()");
+    }
+    void fxn(int b){
+        System.out.println("hello from fxn(int) = "+b);
     }
 }
--- lab/lab.java
+++ lab/lab.java
@@ -1,5 +1,4 @@
-package lab ;
-
+package lab;
 public class lab {
     public static void main(String arg[]){
         System.out.println("hello world \n how are you ");
