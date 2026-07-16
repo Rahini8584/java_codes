6a723aee0808c643b35da7a5081832a50197d71d
Rahini <rahinikalra6@gmail.com>
2026-07-16 15:05:30 +0530

    question 2

--- Student.java
+++ Student.java
@@ -29,16 +29,26 @@ public class Student {
         // sc.close();
 
 
-        
+
+
+
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[5];
-        for(int i = 0 ; i<5 ; i++){
+        int n ; 
+        n=sc.nextInt();
+        int sum= 0 ;
+        int arr[] = new int[n];
+        for(int i = 0 ; i<arr.length ; i++){
         arr[i]=sc.nextInt();
         }
-       for(int i = 0 ; i<5 ; i++){
-        System.out.print(arr[i]+ " ");
+       for(int i = 0 ; i<arr.length; i++){
+        sum +=arr[i];
        }
+       System.out.println(sum);
        sc.close();
+
+
+
+
 
 
 
