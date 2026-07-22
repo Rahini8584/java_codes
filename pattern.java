f89a3ababd795d9229b5219aea1d7a153ec7fef8
Rahini <rahinikalra6@gmail.com>
2026-07-22 10:10:08 +0530

    patten questions

---
+++ pattern.java
@@ -0,0 +1,134 @@
+// import java.util.Scanner ;
+// public class pattern{
+
+//     public static void main(String arg[] ){
+//         int n ;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number");
+//         n=sc.nextInt();
+//         for(int i = 1 ; i<= n ; i++){
+//             for(int j=1 ; j<=i ; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.print("\n");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+
+
+// import java .util.Scanner;
+// public class pattern{
+//     public static void main(String arg[]){
+//         int n ;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number");
+//         n=sc.nextInt();
+//         for(int i = 1 ; i<=n ; i++){
+//             for(int j =n-1 ; j>=i; j--){
+//                 System.out.print("*");
+//             }
+//             for(int k =0 ; k<i ; k++){
+                
+//                 System.out.print(k+1);
+                
+//             }
+//             System.out.print("\n");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+
+// import java .util.Scanner;
+// public class pattern{
+//     public static void main(String arg[]){
+//         int n ;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number");
+//         n=sc.nextInt();
+//         for(int i = 1 ; i<=n ; i++){
+//             for(int j =n-1 ; j>=i; j--){
+//                 System.out.print("*");
+//             }
+            
+//             for(int k=i ; k>0 ; k--){
+//                 System.out.print(k);
+                
+                
+//             }
+//             System.out.print("\n");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+// import java .util.Scanner;
+// public class pattern{
+//     public static void main(String arg[]){
+//         int n ;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number");
+//         n=sc.nextInt();
+//         for(int i = 1 ; i<=n ; i++){
+//             for(int j= i ; j>0; j--){
+//                 System.out.print(j);
+               
+//             }
+//             System.out.print("\n");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+
+// import java .util.Scanner;
+// public class pattern{
+//     public static void main(String arg[]){
+//         int n ;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number");
+//         n=sc.nextInt();
+//         for(int i = 1 ; i<=n ; i++){
+//             for(int j=1 ; j<=i; j++){
+//                 System.out.print(j);
+               
+//             }
+//             System.out.print("\n");
+//         }
+//         sc.close();
+//     }
+// }
+
+
+
+
+// import java .util.Scanner;
+// public class pattern{
+//     public static void main(String arg[]){
+//         int n ;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter a number");
+//         n=sc.nextInt();
+//         for(int i = 1 ; i<=n ; i++){
+//             for(int j =n-1 ; j>=i ; j--){
+//                 System.out.print("*");
+//             }
+            
+//             for(int k = 1 ; k<=i; k++){
+                
+//                 System.out.print(i);
+                
+//             }
+            
+//             System.out.println();
+
+//         }
+//         sc.close();
+//     }
+// }
